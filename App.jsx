153adacc360392ebc7777e7f9f7d0ce94862261f
Lexonleki <karanmuchonge@gmail.com>
2026-06-10 10:49:17 +0300

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App(){
- return (
-  <div style={{padding:'40px',fontFamily:'Arial'}}>
-   <h1>Lewis Karani Gym</h1>
-   <p>Welcome to Lewis Karani Gym. Replace this file with your full gym dashboard code.</p>
-   <p>Phone: 0112529315</p>
-  </div>
- );
-}
